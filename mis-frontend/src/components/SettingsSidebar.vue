eea189d12cfec3543e8dc4dc8d8ce9aca409f9ed
Alexandre Charoy <alexandre.charoy@gmail.com>
2020-10-25 14:37:56 +0100

    Added language setting

--- mis-frontend/src/components/SettingsSidebar.vue
+++ mis-frontend/src/components/SettingsSidebar.vue
@@ -6,6 +6,17 @@
         <span>{{ $t('settings.title') }}</span>
         <i class="el-icon-arrow-right action" @click="closeDrawer" />
       </header>
+      <div class="setting">
+        <div class="setting-title">{{ $t('settings.language') }}</div>
+        <div>
+          <el-select v-model="$i18n.locale" @change="selectLanguage" style="width: 100%" size="small">
+            <el-option v-for="(lang, i) in languages" :key="`Lang${i}`" :label="lang.label" :value="lang.value">
+              <span style="float: left">{{ lang.label }}</span>
+              <span style="float: right; color: #8492a6; font-size: 13px">{{ lang.value }}</span>
+            </el-option>
+          </el-select>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -13,8 +24,15 @@
 <script>
 import { SET_SETTINGS_DRAWER_OPEN } from '@/store/mutations.js';
 
+import languageIndex from '../i18n/index.json';
+
 export default {
   name: 'SettingsSidebar',
+  data() {
+    return {
+      languages: languageIndex.languages,
+    };
+  },
   computed: {
     drawerOpen: function() {
       return this.$store.state.settings.drawerOpen;
@@ -23,6 +41,9 @@ export default {
   methods: {
     closeDrawer: function() {
       return this.$store.commit(SET_SETTINGS_DRAWER_OPEN, false);
+    },
+    selectLanguage: function(language) {
+      localStorage.setItem('language', language);
     },
   },
 };
@@ -52,11 +73,11 @@ export default {
   box-shadow: -1px 0px 2px 0px $mis-color-shadow;
   transition: width 0.5s;
   overflow: hidden;
+  text-align: left;
   &.shown {
     width: 280px;
   }
   & > header {
-    text-align: left;
     display: flex;
     justify-content: space-between;
     background-color: $mis-color-primary;
@@ -65,6 +86,13 @@ export default {
     & > .action:hover {
       cursor: pointer;
     }
+  }
+}
+
+.setting {
+  padding: 16px;
+  & > .setting-title {
+    padding: 8px 0px;
   }
 }
 </style>
--- mis-frontend/src/main.js
+++ mis-frontend/src/main.js
@@ -7,6 +7,7 @@ Vue.config.productionTip = false;
 import Element from 'element-ui';
 import './style/theme.scss';
 import enLocale from 'element-ui/lib/locale/lang/en';
+import deLocale from 'element-ui/lib/locale/lang/de';
 Vue.use(Element, {
   i18n: (key, value) => i18n.t(key, value),
 });
@@ -17,8 +18,10 @@ Vue.use(VueI18n);
 
 // Load internationalization messages
 import enMessages from './i18n/en.json';
+import deMessages from './i18n/de.json';
 const messages = {
   en: Object.assign(enMessages, enLocale),
+  de: Object.assign(deMessages, deLocale),
 };
 
 // Configure internationalization
