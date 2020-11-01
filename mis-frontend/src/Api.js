35d08f42e6c48dbbe63269e03e4e0222cf52584c
Alexandre Charoy <alexandre.charoy@gmail.com>
2020-11-01 15:22:39 +0100

    Added first draft of API client

---
+++ mis-frontend/src/Api.js
@@ -0,0 +1,38 @@
+function processResponse(response) {
+  return new Promise((resolve, reject) => {
+    if (response.status === 204) {
+      resolve({ status: response.status });
+    } else if (response.status === 401 || response.status === 403) {
+      reject({ status: response.status });
+    } else {
+      let handler;
+      response.status < 400 ? (handler = resolve) : (handler = reject);
+      response.json().then(json => handler({ status: response.status, body: json }));
+    }
+  });
+}
+
+function catchNetworkError(response) {
+  return new Promise((_, reject) => {
+    reject({ status: null, message: 'general.networkError' });
+  });
+}
+
+function getAuthorizationHeader() {
+  return 'Bearer ' + localStorage.getItem('token');
+}
+
+const SERVER_ENDPOINT = process.env.VUE_APP_SERVER_ENDPOINT;
+
+export default {
+  getIdentities: (page, elementsPerPage) => {
+    return fetch(`${SERVER_ENDPOINT}/api/v1/identities?page=${page}&elementsPerPage=${elementsPerPage}`, {
+      method: 'GET',
+      withCredentials: true,
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json', Authorization: getAuthorizationHeader() },
+    })
+      .catch(catchNetworkError)
+      .then(processResponse);
+  },
+};
